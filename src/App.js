90dbe0ced3b655665cf24a36a9278ab2a848ed9d
Buhaiov-A <artyombugayov@gmail.com>
2023-06-16 15:16:08 +0200

    second commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import ItemList from "./components/OnlineShop/ItemList";
+import Product from "./components/Product/Product";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <ItemList /> */}
+      <Product />
+    </div>
+  );
+}
+
+export default App;
